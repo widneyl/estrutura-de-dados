f9481a8e286d12d9cb657eb223a8356ee6c6551a
Widney Lima <widneylima21@gmail.com>
2024-03-15 10:57:43 -0300

    Excluindo arquivos inuteis

---
+++ aulas/recursao/ex001.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int somatorio(int n){
+    if(n == 1){
+        return 1;
+    }
+    else{
+        cout << "numero atual: " << n << " somando " << n << " com " << n-1 << " = " << n+n-1 << endl;
+        return somatorio(n-1)+n;
+    }
+}
+
+int main(){
+    int sum = somatorio(10);
+    cout << sum << endl;
+}
