9dd7044b7eb38db5970a748e7371412cb647d360
Widney Lima <widneylima21@gmail.com>
2024-04-02 00:29:52 -0300

    Estudos 02/04/2024

--- aulas _ed/atv/atv02/Coeficientes_Binomiais.cpp
+++ aulas _ed/atv/atv02/Coeficientes_Binomiais.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+void coefB(int n, int p)
+{
+    if (n == 1)
+    {
+        return;
+    }
+    else
+    {
+        int fat =0;
+        for (int i = n-1; n >= 1; i--){
+            for (int j = n; n > 1; j--){
+                fat = j*i;
+            }
+        }
+    }
+}
 
+int fat(int n){
+    if (n == 1){
+        return 1;
+    }
+    else{
+        
+        return n * fat(n-1);
+
+    }
+}
+
+int main()
+{
+   int re = fat(5);
+   cout << re << endl;
+    
 }
